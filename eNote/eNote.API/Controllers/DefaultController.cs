b7560885239db797115278cecfcb74b5a957bea9
Tarik Džakić <tarik.dzakic@outlook.com>
2024-05-17 10:31:40 +0200

    1.0 - Initial commit

---
+++ eNote/eNote.API/Controllers/DefaultController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace eNote.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        
+    }
+}
